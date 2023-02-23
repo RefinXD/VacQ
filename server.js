829861682d0b0ce993f1dc82ee1659dcb547acc7
RefinXD <The32069@gmail.com>
2023-02-23 13:33:31 +0700

    added assignment 6 code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const cookieParser = require('cookie-parser')
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
@@ -10,14 +10,19 @@ connectDB();
 //Route
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 const hospitals = require (`./routes/hospitals`);
+const auth = require('./routes/auth');
+
 app.use(`/api/v1/hospitals`,hospitals)
+app.use('/api/v1/auth',auth)
 
 //Body parser
 const PORT = process.env.PORT || 3000;
-app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,' mode on port ',PORT))
+const server = app.listen(PORT,console.log('Server running in ',process.env.NODE_ENV,' mode on port ',PORT))
 //Handle unhandled promise rejections
-process.on('unhandledRejection',(err,procmise)=>{
+process.on('unhandledRejection',(err,promise)=>{
     console.log(`Error: ${err.message}`);
     //Close server & exit process
     server.close(()=>process.exit(1));
